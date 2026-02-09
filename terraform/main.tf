a45cfa7e3f5f2c70f3cfdb3695200dbe33120f17
Ubuntu <ubuntu@ip-172-31-30-95.ap-south-1.compute.internal>
2026-02-09 06:39:34 +0000

    change public ip location inside jenkins

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,6 +1,6 @@
 resource "aws_key_pair" "devops_key" {
   key_name   = "devops-key"
-  public_key = file("~/.ssh/devops-key.pub")
+  public_key = ~/.ssh/ = /var/lib/jenkins/.ssh/
 }
 
 resource "aws_security_group" "web_sg" {
