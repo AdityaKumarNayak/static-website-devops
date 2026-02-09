31f08c685f6e4d6807dd33244d4af8b0c6db1619
Ubuntu <ubuntu@ip-172-31-30-95.ap-south-1.compute.internal>
2026-02-09 06:50:38 +0000

    corrected the path

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,6 +1,6 @@
 resource "aws_key_pair" "devops_key" {
   key_name   = "devops-key"
-  public_key = ~/.ssh/ = /var/lib/jenkins/.ssh/
+  public_key = file("~/.ssh/devops-key.pub")
 }
 
 resource "aws_security_group" "web_sg" {
